f3ccb50c3d312a034bf14496b5b57ff2999f4c9d
uytruong <uytruong97@gmail.com>
2021-07-10 22:13:42 +0700

    add favicon and Navbar.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { ContentLayoutComponent } from './layouts/content-layout/content-layout.component';
+import { NavbarComponent } from './layouts/content-layout/navbar/navbar.component';
+import { SidebarComponent } from './layouts/content-layout/sidebar/sidebar.component';
+import { SharedModule } from './shared/shared.module';
+
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { UserOutline, SearchOutline, PlusOutline, InfoCircleFill } from '@ant-design/icons-angular/icons';
+
+const NZ_ICONS: IconDefinition[] = [UserOutline, SearchOutline, PlusOutline, InfoCircleFill];
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthLayoutComponent,
-    ContentLayoutComponent
+    ContentLayoutComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule,
+    NzIconModule.forRoot(NZ_ICONS)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, NzAvatarModule, NzToolTipModule, NzPopoverModule],
+  exports: [NzAvatarModule, NzToolTipModule, NzPopoverModule],
 })
-export class SharedModule { }
+export class SharedModule {}
